a2bc364f128ae128370d71d43def09d8093371ec
Sabid Ansari <ansarisabid84@gmail.com>
2023-07-18 15:53:27 +0530

    Designed chats page

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -7,6 +7,29 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
+  chats: any[] = [
+    { "name": "Person One" },
+    { "name": "Person Two" },
+    { "name": "Person Three" },
+    { "name": "Person Four" },
+    { "name": "Person Five" },
+    { "name": "Person Six" },
+    { "name": "Person Seven" },
+    { "name": "Person Eight" },
+    { "name": "Person Nine" },
+    { "name": "Person Ten" },
+    { "name": "Person Eleven" },
+    { "name": "Person Tweleve" },
+    { "name": "Person Thirteen" },
+    { "name": "Person Fourteen" },
+    { "name": "Person Fifteen" },
+    { "name": "Person Sixteen" },
+    { "name": "Person Seventeen" },
+    { "name": "Person Eighteen" },
+    { "name": "Person Nineteen" },
+    { "name": "Person Twenty" }
+  ]
+
   constructor() {}
 
 }
